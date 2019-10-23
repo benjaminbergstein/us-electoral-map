dca80ec6ae9f57fa46d12e3627165ebceba2f229
Benjamin Bergstein <bennyjbergstein+github@gmail.com>
2019-10-23 18:55:45 +0000

    Migrate to yarn + webpack & update JS style.

--- js/application_helper.js
+++ js/application_helper.js
@@ -11,3 +11,5 @@ ApplicationHelper.prototype.forEachState = function(callback) {
 ApplicationHelper.prototype.popupTemplate = function(state, data) {
   return '<span style="color: white; font-weight: 900; text-shadow: 0 0 1px #000, 1px 1px 1px #000;">' + state.properties.name + ": " + data['Electoral Votes'] + '</span>'
 };
+
+export default ApplicationHelper;
---
+++ js/constants.js
@@ -0,0 +1,11 @@
+const Blue = '#ABDFFF'
+const Neutral = '#DADAD0'
+const Red = '#FF0000'
+const PossibleFills = {
+  defaultFill: Neutral,
+  1: Neutral,
+  2: Blue,
+  3: Red,
+}
+
+export { Blue, Neutral, Red, PossibleFills };
